61aff57fbeeb638a15d1756798c16f0cdde7fd50
lidefu <lidefu@iauto.com>
2019-02-27 11:10:35 +0800

    add test

---
+++ function1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def say_hello():
+    print("Hello World")
+
+say_hello()
+say_hello()
+
---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    print("main")
+    str = "0x0a"
+    print(str)
+    h = hex(int(str, 16))
+    print(h)
+    ##print(hex(str)[2:])
